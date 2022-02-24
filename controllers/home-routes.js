aac62e283e5c984bc10ffee9883374c4a4d78509
Kareem Rizk <kareem.magdy5@gmail.com>
2022-02-24 16:44:32 -0500

    server error

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 const sequelize = require('../config/connection');
 const { Post, User, Comment } = require('../models');
-const posts = dbPostData.map(post => post.get({ plain: true }));
+// const posts = dbPostData.map(post => post.get({ plain: true }));
 
 router.get('/', (req, res) => {
     Post.findAll({
@@ -30,12 +30,18 @@ router.get('/', (req, res) => {
       .then(dbPostData => {
         // pass a single post object into the homepage template
         console.log(dbPostData[0]);
+        const posts = dbPostData.map(post => post.get({ plain: true }));
         res.render('homepage', { posts });
     })
       .catch(err => {
         console.log(err);
         res.status(500).json(err);
       });
+  });
+
+
+  router.get('/login', (req, res) => {
+    res.render('login');
   });
 
 module.exports = router;
